f861b48b42644242921c83925a2f10f054611ede
teshome hailu <teshe@2008@hotmail.com>
2020-07-29 21:23:17 +0300

    dashboard added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,67 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Black Dashboard') }}</title>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('black') }}/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="{{ asset('black') }}/img/favicon.png">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+        <!-- Icons -->
+        <link href="{{ asset('black') }}/css/nucleo-icons.css" rel="stylesheet" />
+        <!-- CSS -->
+        <link href="{{ asset('black') }}/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+        <link href="{{ asset('black') }}/css/theme.css" rel="stylesheet" />
+    </head>
+    <body class="{{ $class ?? '' }}">
+        @auth()
+            <div class="wrapper">
+                    @include('layouts.navbars.sidebar')
+                <div class="main-panel">
+                    @include('layouts.navbars.navbar')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <div class="content">
+                        @yield('content')
+                    </div>
                 </div>
             </div>
-        </nav>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        @else
+            @include('layouts.navbars.navbar')
+            <div class="wrapper wrapper-full-page">
+                <div class="full-page {{ $contentClass ?? '' }}">
+                    <div class="content">
+                        <div class="container">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endauth
+        </div>
+        <script src="{{ asset('black') }}/js/core/jquery.min.js"></script>
+        <script src="{{ asset('black') }}/js/core/popper.min.js"></script>
+        <script src="{{ asset('black') }}/js/core/bootstrap.min.js"></script>
+        <script src="{{ asset('black') }}/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+        <!--  Google Maps Plugin    -->
+        <!-- Place this tag in your head or just before your close body tag. -->
+        {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> --}}
+        <!-- Chart JS -->
+        {{-- <script src="{{ asset('black') }}/js/plugins/chartjs.min.js"></script> --}}
+        <!--  Notifications Plugin    -->
+        <script src="{{ asset('black') }}/js/plugins/bootstrap-notify.js"></script>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        <script src="{{ asset('black') }}/js/black-dashboard.min.js?v=1.0.0"></script>
+        <script src="{{ asset('black') }}/js/theme.js"></script>
+
+    </body>
 </html>
